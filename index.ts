12f1ce362a7ffd3afe28f38bbe797fdeb057d37f
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2026-03-17 01:32:08 +0800

    fix: update repository URL in package.json

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Filter, Handler, param, Types, UserModel } from 'hydrooj';
-export async function apply(ctx: Context) {
-    await UserModel.coll.updateOne(
-        { uid: 9, domainId: 'system' },
-        { $set: { join: true } },
-        { upsert: true }
-    );
-}
