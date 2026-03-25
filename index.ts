e3e97268f23c74bda62c77dc683bc57ee6c5bb0a
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2026-03-26 03:16:26 +0800

    feat: add navigation link for '程設班官網' in docspage

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Filter, Handler, param, Types, UserModel } from 'hydrooj';
-
-export async function apply(ctx: Context) {
-    ctx.injectUI('Nav', 'docs_page', (handler: Handler) => ({
-        icon: 'info',
-        displayName: '程設班官網',
-        link: '/homepage/',
-    }));
-}
