864974290247708f4b49ff691715301c10154894
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2026-03-26 02:52:10 +0800

    fix: update UI injection context from 'homepage' to 'docs'

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Context, Filter, Handler, param, Types, UserModel } from 'hydrooj';
 
 export async function apply(ctx: Context) {
-    ctx.injectUI('Nav', 'homepage', (handler: Handler) => ({
+    ctx.injectUI('Nav', 'docs', (handler: Handler) => ({
         icon: 'info',
         displayName: '程設班官網',
         link: '/homepage/',
